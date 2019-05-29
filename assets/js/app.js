fa0927f356fbc2b3773a6eed67f2142ca94b7d1f
icepants <shawnluther99@gmail.com>
2019-05-29 02:22:36 -0400

    Styled the jQuery generated questions.

--- assets/js/app.js
+++ assets/js/app.js
@@ -56,22 +56,23 @@ $(document).ready(function () {
             alert("LOSS!");
         }
 
+
         // This function assembles the question we will display in HTML.
         var buildHTMLQuestions = function (i) {
             // Display the question according to the position of our iterator in the 'triviaQuestions' array.
             $('#questionPopulation').append('<div id="playerQuestion" class="text-info"><h4>' + triviaQuestions[i].question + '</div>');
             // Display the multiple choice answers according to the position of our iterator in the 'triviaQuestions' array.
-            $('#choicePopulation').append('<input type="radio" name="multipleChoice" value="' + triviaQuestions[i].choices[0] + '" checked="yes">' + triviaQuestions[i].choices[0] + '<br>');
-            $('#choicePopulation').append('<input type="radio" name="multipleChoice" value="' + triviaQuestions[i].choices[1] + '">' + triviaQuestions[i].choices[1] + '<br>');
-            $('#choicePopulation').append('<input type="radio" name="multipleChoice" value="' + triviaQuestions[i].choices[2] + '">' + triviaQuestions[i].choices[2] + '<br>');
-            $('#choicePopulation').append('<input type="radio" name="multipleChoice" value="' + triviaQuestions[i].choices[3] + '">' + triviaQuestions[i].choices[3] + '<br> <br>');
+            $('#choicePopulation').append('<label class="label_item" for="radio1"><img width="60" height="60" src="assets/images/decorations/orangeSoda.png"></label> <input type="radio" id="radio1" class="radio_item" name="multipleChoice" value="' + triviaQuestions[i].choices[0] + '" checked="yes"><span class="text-warning font-weight-bold"> ' + triviaQuestions[i].choices[0] + '</span><br>');
+            $('#choicePopulation').append('<label class="label_item" for="radio2"><img width="60" height="60" src="assets/images/decorations/orangeSoda.png"></label> <input type="radio" id="radio2" class="radio_item" name="multipleChoice" value="' + triviaQuestions[i].choices[1] + '" checked="yes"><span class="text-warning font-weight-bold"> ' + triviaQuestions[i].choices[1] + '</span><br>');
+            $('#choicePopulation').append('<label class="label_item" for="radio3"><img width="60" height="60" src="assets/images/decorations/orangeSoda.png"></label> <input type="radio" id="radio3" class="radio_item" name="multipleChoice" value="' + triviaQuestions[i].choices[2] + '" checked="yes"><span class="text-warning font-weight-bold"> ' + triviaQuestions[i].choices[2] + '</span><br>');
+            $('#choicePopulation').append('<label class="label_item" for="radio4"><img width="60" height="60" src="assets/images/decorations/orangeSoda.png"></label> <input type="radio" id="radio4" class="radio_item" name="multipleChoice" value="' + triviaQuestions[i].choices[3] + '" checked="yes"><span class="text-warning font-weight-bold"> ' + triviaQuestions[i].choices[3] + '</span><br> <br>');
             // 5.) Once the player picks the answer they believe to be correct, let the player submit it.
             // Submit Button.
-            $('#submitPopulation').append('<button type="submit" id="submitButton">Submit</button>');
+            $('#submitPopulation').append('<button type="submit" class="btn btn-warning text-white p-3 font-weight-bold" id="submitButton">Submit</button>');
 
             // 6.) If the player picks the correct answer, let them know, wait five seconds, then continue.
             $('#submitButton').click(function () {
-                if ($('input:radio[name=multipleChoice]:checked').val() === triviaQuestions[i].correctAnswer && i < 4) {
+                if ($('input:radio[name=multipleChoice]:checked').val() === triviaQuestions[i].correctAnswer) {
                     playerCorrect();
                 } else {
                     // 7.) If the players answer is incorrect, let them know, wait five seconds, then continue.
@@ -81,6 +82,11 @@ $(document).ready(function () {
         }
         // Start the question array iterator at zero to properly define which question to start with in the array.
         buildHTMLQuestions(questionNumber);
+
+        // When the player clicks on an orange soda, turn it into a checkmark.
+        $("img").on("click", function () {
+            $(this).attr('src', 'assets/images/decorations/tick.png');
+        });
     }
 });
 
